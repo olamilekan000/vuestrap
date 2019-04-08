3e5d5ab85abce1e22eb1ba87788df7d5210a9f9b
Olamilekan Odukoya <olarpringo@gmail.com>
2019-04-08 03:10:51 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue)
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,5 +14,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  BootstrapVue,
   render: h => h(App),
 }).$mount('#app');
